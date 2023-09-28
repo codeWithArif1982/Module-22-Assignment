a68e067029a81de60f61e0cae681ed9adf2f8925
codeWithArif1982 <125276843+codeWithArif1982@users.noreply.github.com>
2023-09-27 23:39:39 -0400

    first commit

---
+++ src/Controllers/subAdminController.js
@@ -0,0 +1,13 @@
+exports.create = async (req, res) => { };
+
+
+exports.read = async (req, res) => { };
+
+
+exports.delete = async (req, res) => { };
+
+
+exports.update = async (req, res) => { };
+
+
+ 
---
+++ src/Routes/api.js
@@ -0,0 +1,7 @@
+/*
+The Routes folder will contain the api.js file.
+And router path should be created in this file.
+Since there will be 13 controller files and each file will have 4 functions,
+a total of 52 router paths will be created.
+All APIs must be made through GET request.
+*/
